21957c38e2829d1a376a2f1fc69f300d4afa5cad
erictarn <erictarn@gmail.com>
2026-01-18 18:44:10 -0800

    Fix zone cache initializer to skip during asset precompilation

--- config/initializers/zone_distribution_cache.rb
+++ config/initializers/zone_distribution_cache.rb
@@ -1,9 +1,14 @@
 Rails.application.config.after_initialize do
-  unless Rails.env.test? || defined?(Rails::Console) || File.basename($0) == 'rake'
-    # Run synchronously to have cache ready immediately
-    result = HeartRate.zone_percentage_distribution_sql
-    Rails.cache.write("heart_rate_zone_percentage_distribution", result, expires_in: 1.hour)
-    Rails.cache.write("heart_rate_zone_percentage_distribution_updated_at", Time.current, expires_in: 1.hour)
-    Rails.logger.info "Zone distribution cache warmed up on startup: #{result}"
+  # Skip during asset precompilation or when database doesn't exist
+  unless Rails.env.test? || defined?(Rails::Console) || File.basename($0) == 'rake' || ENV['SECRET_KEY_BASE_DUMMY'].present?
+    begin
+      # Run synchronously to have cache ready immediately
+      result = HeartRate.zone_percentage_distribution_sql
+      Rails.cache.write("heart_rate_zone_percentage_distribution", result, expires_in: 1.hour)
+      Rails.cache.write("heart_rate_zone_percentage_distribution_updated_at", Time.current, expires_in: 1.hour)
+      Rails.logger.info "Zone distribution cache warmed up on startup: #{result}"
+    rescue ActiveRecord::StatementInvalid, ActiveRecord::NoDatabaseError => e
+      Rails.logger.warn "Skipping zone distribution cache warmup: #{e.message}"
+    end
   end
 end
